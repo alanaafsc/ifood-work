411170264353b32ff78463c8b6d9070aac25875c
alanaafsc <alana.ingrid@ufpe.br>
2022-11-03 16:49:34 -0300

    Protegendo endpoints e atualizando OpenAPI

--- ifood/cadastro/src/main/java/com/github/alanaafsc/ifood/cadastro/PratoResource.java
+++ ifood/cadastro/src/main/java/com/github/alanaafsc/ifood/cadastro/PratoResource.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
@@ -20,6 +21,10 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlow;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlows;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import com.github.alanaafsc.ifood.cadastro.dto.AdicionarPratoDTO;
@@ -31,6 +36,8 @@ import com.github.alanaafsc.ifood.cadastro.dto.PratoMapper;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @Tag(name = "Prato")
+@RolesAllowed("proprietario")
+@SecurityScheme(securitySchemeName = "ifood-oauth", type = SecuritySchemeType.OAUTH2, flows = @OAuthFlows(password = @OAuthFlow(tokenUrl = "http://localhost:8180/auth/realms/ifood/protocol/openid-connect/token")))
 public class PratoResource {
 	
 	@Inject
