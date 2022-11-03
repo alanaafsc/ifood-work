6c096ae53e864772313ec03bd8bfc399bfcdf275
alanaafsc <alana.ingrid@ufpe.br>
2022-11-03 09:46:01 -0300

    Update RestauranteResourceTest.java

--- ifood/cadastro/src/test/java/com/github/alanaafsc/ifood/cadastro/RestauranteResourceTest.java
+++ ifood/cadastro/src/test/java/com/github/alanaafsc/ifood/cadastro/RestauranteResourceTest.java
@@ -1,6 +1,8 @@
 package com.github.alanaafsc.ifood.cadastro;
 
-import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.ws.rs.core.Response.Status;
 
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,9 @@ import com.github.database.rider.core.api.dataset.DataSet;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 
 @DBRider
 @DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE)
@@ -30,5 +35,29 @@ public class RestauranteResourceTest {
        Approvals.verifyJson(resultado);
     
     }
-
+    
+    private RequestSpecification given(){
+    	return RestAssured.given().contentType(ContentType.JSON);
+    }
+    
+    @Test
+    @DataSet("restaurantes-cenario-1.yml")
+    public void testAtualizarRestaurante() {
+    	Restaurante dto = new Restaurante();
+    	dto.nome = "novoNome";
+    	Long parameterValue = 123L;
+    	given()
+		    	.with().pathParam("id", parameterValue)
+		    	.body(dto)
+		    	.when().put("/restaurantes/{id}")
+		    	.then().statusCode(Status.NO_CONTENT.getStatusCode())
+		    	.extract().asString();
+    	Restaurante findById = Restaurante.findById(parameterValue);
+    	
+    	//poderia testar todos os outros atributos
+    	assertEquals(dto.nome, findById.nome);
+    	
+	    	
+    }
+    
 }
