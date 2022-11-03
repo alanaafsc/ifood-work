edbf74172f8d68f768f0bd8ed79cfb7072a88300
alanaafsc <alana.ingrid@ufpe.br>
2022-11-03 11:10:40 -0300

    Validação utilizando o Hibernate Validator

--- ifood/cadastro/src/main/java/com/github/alanaafsc/ifood/cadastro/dto/AdicionarRestauranteDTO.java
+++ ifood/cadastro/src/main/java/com/github/alanaafsc/ifood/cadastro/dto/AdicionarRestauranteDTO.java
@@ -1,11 +1,18 @@
 package com.github.alanaafsc.ifood.cadastro.dto;
 
-public class AdicionarRestauranteDTO {
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+public class AdicionarRestauranteDTO {
+	
+	@NotEmpty
+	@NotNull
 	public String proprietario;
 
 	public String cnpj;
-
+	
+	@Size(min = 3, max = 30)
 	public String nomeFantasia;
 
 	public LocalizacaoDTO localizacao;
