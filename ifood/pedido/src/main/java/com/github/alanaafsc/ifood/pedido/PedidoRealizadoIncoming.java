113b8f90be20e327f80d83e5b8e35968945849c2
alanaafsc <alana.ingrid@ufpe.br>
2022-11-14 11:10:53 -0300

    Salvando Pedidos Recebidos no MongoDB

--- ifood/pedido/src/main/java/com/github/alanaafsc/ifood/pedido/PedidoRealizadoIncoming.java
+++ ifood/pedido/src/main/java/com/github/alanaafsc/ifood/pedido/PedidoRealizadoIncoming.java
@@ -1,9 +1,11 @@
 package com.github.alanaafsc.ifood.pedido;
 
 import io.smallrye.common.annotation.Blocking;
+import org.bson.types.Decimal128;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.ArrayList;
 
 @ApplicationScoped
 public class PedidoRealizadoIncoming {
@@ -13,5 +15,22 @@ public class PedidoRealizadoIncoming {
     public void lerPedidos(PedidoRealizadoDTO dto){
         System.out.println("---------------------");
         System.out.println(dto);
+
+        Pedido p = new Pedido();
+        p.cliente = dto.cliente;
+        p.pratos = new ArrayList<>();
+        dto.pratos.forEach(prato -> p.pratos.add(from(prato)));
+        Restaurante restaurante = new Restaurante();
+        restaurante.nome = dto.restaurante.nome;
+        p.restaurante = restaurante;
+        p.persist();
+    }
+
+    private Prato from(PratoPedidoDTO prato) {
+        Prato p = new Prato();
+        p.descricao = prato.descricao;
+        p.nome = prato.nome;
+        p.preco = new Decimal128(prato.preco);
+        return p;
     }
 }
